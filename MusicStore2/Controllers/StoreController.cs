de7c78e83cbe587b46ab1bba57652b211aee8483
Robooto <samrussell63@gmail.com>
2015-03-17 22:57:52 -0700

    First Commit
    Learning .Net MCV with simple store example

---
+++ MusicStore2/Controllers/StoreController.cs
@@ -0,0 +1,46 @@
+﻿using MusicStore2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore2.Controllers
+{
+    public class StoreController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            var genres = storeDB.Genres.ToList();
+
+            return View(genres);
+        }
+
+        //
+        // GET: /Store/Browse
+
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = storeDB.Genres.Include("Albums")
+                .Single(g => g.Name == genre);
+
+            return View(genreModel);
+        }
+
+        //
+        // GET: /Store/Browse
+
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album " + id };
+
+            return View(album);
+        }
+
+    }
+}
---
+++ MusicStore2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MusicStore2.MvcApplication" Language="C#" %>
